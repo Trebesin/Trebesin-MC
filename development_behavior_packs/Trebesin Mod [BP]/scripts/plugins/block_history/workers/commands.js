4e2454bb083cc6ccac8905fddd1cbaeebe1f171d
tpkowastaken <protitom@gmail.com>
2022-12-14 00:05:23 +0100

    seems better - bh inspect

--- development_behavior_packs/Trebesin Mod [BP]/scripts/plugins/block_history/workers/commands.js
+++ development_behavior_packs/Trebesin Mod [BP]/scripts/plugins/block_history/workers/commands.js
@@ -3,8 +3,8 @@ import {CommandParser, sendMessage} from "../../../mc_modules/commandParser";
 import { command_parser, isAdmin } from "../../commands/workers/admin";
 import { exported } from "../block_history";
 function main(){
-  async function blockHistoryHandler(sender, parameter){
-        sendMessage("hello", sender, "bh")
+    async function blockHistoryHandler(sender, parameter){
+      sendMessage("hello", "§aCMD§f", sender)
         /*
         if(isAdmin(sender) && (parameter.command === "inspect" || parameter.command === "i")){
             const request = {
@@ -12,7 +12,7 @@ function main(){
             }
             await sendMessage(exported.connection.query(request).stringify,"bh",sender)
         }*/
-  }
+    }
   command_parser.registerCommand("bh", {
     parameters: [{id: "distance", type: "int", optional: true}], aliases: ["block_history"], run: blockHistoryHandler
   })
