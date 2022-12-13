40a9716129d6241f25c4c337f2e827de178b94a8
tpkowastaken <protitom@gmail.com>
2022-12-14 00:06:40 +0100

    block history inspect functional?

--- development_behavior_packs/Trebesin Mod [BP]/scripts/plugins/block_history/workers/commands.js
+++ development_behavior_packs/Trebesin Mod [BP]/scripts/plugins/block_history/workers/commands.js
@@ -4,17 +4,16 @@ import { command_parser, isAdmin } from "../../commands/workers/admin";
 import { exported } from "../block_history";
 function main(){
     async function blockHistoryHandler(sender, parameter){
-      sendMessage("hello", "§aCMD§f", sender)
-        /*
+        world.say(parameter.command)
         if(isAdmin(sender) && (parameter.command === "inspect" || parameter.command === "i")){
             const request = {
                 sql : `Select * FROM 'block_history' WHERE x = ${sender.location.x} AND y = ${sender.location.y} AND z = ${sender.location.z}`
             }
             await sendMessage(exported.connection.query(request).stringify,"bh",sender)
-        }*/
+        }
     }
   command_parser.registerCommand("bh", {
-    parameters: [{id: "distance", type: "int", optional: true}], aliases: ["block_history"], run: blockHistoryHandler
+    parameters: [{id: "command", type: "string", optional: true}], aliases: ["block_history"], run: blockHistoryHandler
   })
 }
 
