aef6f9f284f002756995d532fa3108e40de6f651
tpkowastaken <protitom@gmail.com>
2022-12-14 00:59:28 +0100

    testing bh i nan behaviour

--- development_behavior_packs/Trebesin Mod [BP]/scripts/plugins/block_history/workers/commands.js
+++ development_behavior_packs/Trebesin Mod [BP]/scripts/plugins/block_history/workers/commands.js
@@ -17,7 +17,9 @@ function main(){
                 for(const block_alteration of response.result){
                     var hey = true
                     for (const player of world.getPlayers()) {
+                        world.say(block_alteration.tick)
                         const timeOfBlockAlteration = system.tick - parseInt(block_alteration.tick)
+                        world.say(timeOfBlockAlteration)
                         if (player.id === block_alteration.actor_id) {
                             sendMessage(`${player.name}: ${block_alteration.before_id} -> ${block_alteration.after_id} - before: ${Math.floor(timeOfBlockAlteration/tickInADay)}d${Math.floor(timeOfBlockAlteration/tickInAnOur)}h${Math.floor(timeOfBlockAlteration/tickInAMin)}m${Math.floor(timeOfBlockAlteration/tickInASec)}s`,'CMD',sender);
                             hey = false; //im sure this can be done better but i dont care at this point
