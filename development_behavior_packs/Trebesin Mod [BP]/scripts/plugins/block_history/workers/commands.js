4d9f6439cfab491f88932afb896627213588687b
tpkowastaken <protitom@gmail.com>
2022-12-13 23:29:14 +0100

    debugging bh inspect - 1.

--- development_behavior_packs/Trebesin Mod [BP]/scripts/plugins/block_history/workers/commands.js
+++ development_behavior_packs/Trebesin Mod [BP]/scripts/plugins/block_history/workers/commands.js
@@ -3,6 +3,7 @@ import {CommandParser, sendMessage} from "../../../mc_modules/commandParser";
 import { command_parser, isAdmin } from "../../commands/workers/admin";
 import { exported } from "../block_history";
 async function blockHistoryHandler(sender, parameter){
+    sendMessage("hello", sender, "bh")
     if(isAdmin(sender) && (parameter.command === "inspect" || parameter.command === "i")){
         const request = {
             sql : `Select * FROM 'block_history' WHERE x = ${sender.location.x} AND y = ${sender.location.y} AND z = ${sender.location.z}`
