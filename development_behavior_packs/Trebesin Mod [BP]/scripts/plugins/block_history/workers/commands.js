57099837c4f5ca78273b8c8bbd2dccfa20dad775
tpkowastaken <protitom@gmail.com>
2022-12-13 23:42:39 +0100

    bh inspect better solution this time

--- development_behavior_packs/Trebesin Mod [BP]/scripts/plugins/block_history/workers/commands.js
+++ development_behavior_packs/Trebesin Mod [BP]/scripts/plugins/block_history/workers/commands.js
@@ -2,19 +2,18 @@ import {CommandResult, MinecraftEffectTypes , system, world, BlockLocation} from
 import {CommandParser, sendMessage} from "../../../mc_modules/commandParser";
 import { command_parser, isAdmin } from "../../commands/workers/admin";
 import { exported } from "../block_history";
-function blockHistoryHandler(sender, parameter){
-    sendMessage("hello", sender, "bh")
-    /*
-    if(isAdmin(sender) && (parameter.command === "inspect" || parameter.command === "i")){
-        const request = {
-            sql : `Select * FROM 'block_history' WHERE x = ${sender.location.x} AND y = ${sender.location.y} AND z = ${sender.location.z}`
-        }
-        await sendMessage(exported.connection.query(request).stringify,"bh",sender)
-    }*/
-}
 function main(){
+    function blockHistoryHandler(sender, parameter){
+        sendMessage("hello", sender, "bh")
+        /*
+        if(isAdmin(sender) && (parameter.command === "inspect" || parameter.command === "i")){
+            const request = {
+                sql : `Select * FROM 'block_history' WHERE x = ${sender.location.x} AND y = ${sender.location.y} AND z = ${sender.location.z}`
+            }
+            await sendMessage(exported.connection.query(request).stringify,"bh",sender)
+        }*/
+    }
     command_parser.registerCommand("bh", {aliases: ["blockhistory", "coreprotect", "co"], parameters: [{id: "command", type: "string", optional: "true"}], run: blockHistoryHandler})
-    command_parser.registerCommand("op", {parameters: [{id: "player", type: "string", optional: true}], senderCheck: isAdmin, run: op})
 }
 
 export {main}
