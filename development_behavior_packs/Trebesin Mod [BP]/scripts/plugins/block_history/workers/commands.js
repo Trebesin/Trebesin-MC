78ba49f5227a74a0b0d94b40ae679ed10f3ec6f5
tpkowastaken <protitom@gmail.com>
2022-12-14 01:12:49 +0100

    almost done - block_history

--- development_behavior_packs/Trebesin Mod [BP]/scripts/plugins/block_history/workers/commands.js
+++ development_behavior_packs/Trebesin Mod [BP]/scripts/plugins/block_history/workers/commands.js
@@ -19,7 +19,7 @@ function main(){
                     const timeOfBlockAlteration = system.currentTick - parseInt(block_alteration.tick)
                     for (const player of world.getPlayers()) {
                         if (player.id === block_alteration.actor_id) {
-                            sendMessage(`${player.name}: ${block_alteration.before_id} -> ${block_alteration.after_id} - before: ${Math.floor(timeOfBlockAlteration/tickInADay)}d${Math.floor(timeOfBlockAlteration/tickInAnOur)}h${Math.floor(timeOfBlockAlteration/tickInAMin)}m${Math.floor(timeOfBlockAlteration/tickInASec)}s`,'CMD',sender);
+                            sendMessage(`${player.name}: ${block_alteration.before_id} -> ${block_alteration.after_id} - before: ${Math.floor(timeOfBlockAlteration/tickInADay)}d${Math.floor(timeOfBlockAlteration%tickInADay/tickInAnOur)}h${Math.floor(timeOfBlockAlteration%tickInAnOur/tickInAMin)}m${Math.floor(timeOfBlockAlteration%tickInAMin/tickInASec)}s`,'CMD',sender);
                             hey = false; //im sure this can be done better but i dont care at this point
                             break;
                         }
