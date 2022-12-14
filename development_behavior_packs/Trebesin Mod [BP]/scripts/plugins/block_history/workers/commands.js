9f9ed16ffec5bac51b22ffe39afdc0dbdf635dba
tpkowastaken <protitom@gmail.com>
2022-12-14 01:05:56 +0100

    another testing

--- development_behavior_packs/Trebesin Mod [BP]/scripts/plugins/block_history/workers/commands.js
+++ development_behavior_packs/Trebesin Mod [BP]/scripts/plugins/block_history/workers/commands.js
@@ -18,6 +18,7 @@ function main(){
                     var hey = true
                     for (const player of world.getPlayers()) {
                         world.say(JSON.stringify(block_alteration.tick))
+                        world.say(system.tick)
                         const timeOfBlockAlteration = parseInt(system.tick) - parseInt(JSON.stringify(block_alteration.tick))
                         if (player.id === block_alteration.actor_id) {
                             sendMessage(`${player.name}: ${block_alteration.before_id} -> ${block_alteration.after_id} - before: ${Math.floor(timeOfBlockAlteration/tickInADay)}d${Math.floor(timeOfBlockAlteration/tickInAnOur)}h${Math.floor(timeOfBlockAlteration/tickInAMin)}m${Math.floor(timeOfBlockAlteration/tickInASec)}s`,'CMD',sender);
