66a13b661a07efaf2a5d31aa0402039e71dada7d
PavelDobCZ23 <pavel23dob@gmail.com>
2022-12-31 21:30:01 +0100

    Added Universal Command Item

--- development_behavior_packs/Trebesin Mod [BP]/scripts/plugins/commands/workers/items.js
+++ development_behavior_packs/Trebesin Mod [BP]/scripts/plugins/commands/workers/items.js
@@ -1,5 +1,5 @@
 import { command_parser } from './admin';
-import {world} from '@minecraft/server';
+import {world,ItemTypes, ItemStack} from '@minecraft/server';
 
 function main() {
     const itemCommands = {
@@ -8,10 +8,41 @@ function main() {
             parameter: '2'
         }
     }
+    command_parser.registerCommand('commanditem',{
+        parameters: [
+            {
+                type: 'str',
+                id: 'commandName'
+            },
+            {
+                type: 'str',
+                id: 'commandParameters'
+            }
+        ],
+        /**
+         * 
+         * @param {Player} sender 
+         * @param {*} parameters 
+         */
+        run(sender,parameters) {
+            const commandItem = new ItemStack(ItemTypes.get('trebesin:cmd_universal'),1);
+            commandItem.setLore([parameters.commandName,parameters.commandParameters]);
+            sender.getComponent('inventory').container.addItem(commandItem);
+        }
+    });
+
     world.events.beforeItemUse.subscribe((eventData) => {
+        //## Dynamic item:
+        if (eventData.item.typeId === 'trebesin:cmd_universal') {
+            const itemLore = eventData.item.getLore();
+            const command = itemLore[0];
+            const parameters = itemLore[1];
+            command_parser.runCommand(command,parameters,eventData.source);
+        }
+        //## Hardcoded items:
         const itemCommand = itemCommands[eventData.item.typeId];
         if (itemCommand) {
-            command_parser.runCommand(itemCommand.input,[itemCommand.parameter],eventData.source);
+            command_parser.runCommand(itemCommand.input,itemCommand.parameter,eventData.source);
         }
     })
 }
