5589f6b20ae72d87ea22bc14fdfdaf2d1ea8448d
tpkowastaken <protitom@gmail.com>
2023-01-15 19:04:12 +0100

    hmmmmmmmmmm

--- development_behavior_packs/Trebesin Mod [BP]/scripts/plugins/block_history/workers/commands.js
+++ development_behavior_packs/Trebesin Mod [BP]/scripts/plugins/block_history/workers/commands.js
@@ -6,14 +6,12 @@ import * as BlockHistoryPLugin from "../block_history";
 function main(){
     async function blockHistoryHandler(sender, parameter){
         if(/*isAdmin(sender) && */(parameter.command === "b" || parameter.command === "block")){
-            const x = parameter.param1 ?? sender.location.x
-            const y = parameter.param2 ?? sender.location.y
-            const z = parameter.param3 ?? sender.location.z
+            const pos = parameter.coords ?? sender.location
             const request = {
                 sql : `SELECT *, PlayerConnections.PlayerName 
                        FROM \`block_history\` 
                        JOIN PlayerConnections ON block_history.actor_id = PlayerConnections.PlayerID 
-                       WHERE x = ${Math.floor(x)} AND y = ${Math.floor(y)} AND z = ${Math.floor(z)}`
+                       WHERE x = ${Math.floor(pos.x)} AND y = ${Math.floor(pos.y)} AND z = ${Math.floor(pos.z)}`
             }
             try {
                 const response = await BlockHistoryPLugin.database.query(request);
@@ -26,7 +24,7 @@ function main(){
                     sendMessage(`${block_alteration.PlayerName}: ${block_alteration.before_id} -> ${block_alteration.after_id} - before: ${Math.floor(timeOfBlockAlteration/tickInADay)}d${Math.floor(timeOfBlockAlteration%tickInADay/tickInAnHour)}h${Math.floor(timeOfBlockAlteration%tickInAnHour/tickInAMin)}m${Math.floor(timeOfBlockAlteration%tickInAMin/tickInASec)}s`,'CMD - BlockHistory',sender);
                 }
                 if(response.result == ""){
-                    sendMessage(`No changes were made to block  ${Math.floor(x)}, ${Math.floor(y)}, ${Math.floor(z)}`,'CMD - BlockHistory',sender);
+                    sendMessage(`No changes were made to block  ${Math.floor(pos.x)}, ${Math.floor(pos.y)}, ${Math.floor(pos.z)}`,'CMD - BlockHistory',sender);
                 }
             }
             catch(error) {
@@ -45,7 +43,23 @@ function main(){
         }
     }
   command_parser.registerCommand("bh", {
-    parameters: [{id: "command", type: "string", optional: true}, {id: "param1", type: "string", optional: true}, {id: "param2", type: "string", optional: true}, {id: "param3", type: "string", optional: true}], aliases: ["block_history", "co", "coreprotect"], run: blockHistoryHandler
+    parameters: [
+        {id: "command", type: "string", optional: true, choice: {
+                b: [
+                    {type:'pos',id:'coords',optional:true}
+                ],
+                block: [
+                    {}
+                ],
+                p: [
+                    {}
+                ],
+                player: [
+                    {}
+                ]
+            }
+        }
+    ], aliases: ["block_history", "co", "coreprotect"], run: blockHistoryHandler
   })
 }
 
