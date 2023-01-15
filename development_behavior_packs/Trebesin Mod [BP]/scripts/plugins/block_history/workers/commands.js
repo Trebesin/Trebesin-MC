4ff40f05aa47171c170020ad25e6576b7447d036
tpkowastaken <protitom@gmail.com>
2023-01-15 17:11:16 +0100

    tf

--- development_behavior_packs/Trebesin Mod [BP]/scripts/plugins/block_history/workers/commands.js
+++ development_behavior_packs/Trebesin Mod [BP]/scripts/plugins/block_history/workers/commands.js
@@ -25,6 +25,7 @@ function main(){
                     const timeOfBlockAlteration = system.currentTick - parseInt(block_alteration.tick)
                     sendMessage(`${block_alteration.PlayerName}: ${block_alteration.before_id} -> ${block_alteration.after_id} - before: ${Math.floor(timeOfBlockAlteration/tickInADay)}d${Math.floor(timeOfBlockAlteration%tickInADay/tickInAnHour)}h${Math.floor(timeOfBlockAlteration%tickInAnHour/tickInAMin)}m${Math.floor(timeOfBlockAlteration%tickInAMin/tickInASec)}s`,'CMD - BlockHistory',sender);
                 }
+                sendMessage(response.result,'CMD - BlockHistory',sender);
                 if(!response){//not working for some reason
                     sendMessage(`No changes were made to this block (since porting it to bedrock)`,'CMD - BlockHistory',sender);
                 }
