3a9740c0f9e21eb05799e92f596e4f0d9c76ce29
PavelDobCZ23 <pavel23dob@gmail.com>
2023-01-18 18:56:19 +0100

    Fixed particles hopefully

--- development_behavior_packs/Trebesin Mod [BP]/scripts/plugins/block_history/workers/commands.js
+++ development_behavior_packs/Trebesin Mod [BP]/scripts/plugins/block_history/workers/commands.js
@@ -22,7 +22,7 @@ function removeAllActiveParticles(){
 }
 function spawnParticles(particleLocation, sender){
     let molang = new MolangVariableMap();
-        molang.setColorRGB('variable.colour',new Color(255,0,0,1));
+        molang.setColorRGB('variable.colour',new Color(1,0,0,1));
     const dimension = world.getDimension('overworld')
     dimension.spawnParticle('trebesin:selection_dot',particleLocation,molang);
 }
