374e9941c9097449a3cf2b9355f3ba8aa3e84f6c
tpkowastaken <protitom@gmail.com>
2023-01-28 23:52:18 +0100

    starting work and wanna sync

--- development_behavior_packs/Trebesin Mod [BP]/scripts/plugins/block_history/workers/commands.js
+++ development_behavior_packs/Trebesin Mod [BP]/scripts/plugins/block_history/workers/commands.js
@@ -1,11 +1,13 @@
 import {CommandResult, MinecraftEffectTypes , world, BlockLocation,Location, TicksPerDay, TicksPerSecond, Vector, MolangVariableMap, Color, system} from "@minecraft/server";
 import {CommandParser, sendMessage} from "../../../mc_modules/commandParser";
 import { getEdgeLocations, createLocationSet2, locationToString, stringToLocation } from "../../../mc_modules/particles";
+import * as Backend from "../../../mc_modules/server" ;
 import { command_parser, isAdmin } from "../../commands/workers/admin";
 import { logMessage } from "../../debug/debug";
 import { playerData } from "../../server/server";
 import * as BlockHistoryPlugin from "../block_history";
 let particlesPerPlayers = {}
+let confirmationPerPlayer = {}
 
 function addActiveParticles(particleLocation, axis, sender) {
     particlesPerPlayers[sender.id] ??= {
@@ -30,13 +32,17 @@ function spawnParticles(location, particleAxis, sender) {
     const dimension = world.getDimension('overworld');
     dimension.spawnParticle(`trebesin:edge_highlight_${particleAxis}`, location, molang);
 }
-function main() {
+function reverseblocks(){}
+function main(){
     system.runSchedule(() => {
         for (const player in particlesPerPlayers) {
-            for (const locationString of particlesPerPlayers[player].particleLocations) {
+            //particles
+            const set = particlesPerPlayers[player].particleLocations;
+            for(const locationString of particlesPerPlayers[player].particleLocations){
                 const particleLocation = stringToLocation(locationString);
                 spawnParticles(particleLocation[0], particleLocation[1], particlesPerPlayers[player].player)
             }
+            //confirmations
         }
     },4);
     async function blockHistoryHandler(sender, parameter){
@@ -119,17 +125,62 @@ function main() {
                 sendMessage(`${error}`,'CMD - BlockHistory',sender);
             }
         }
-        else if (/*isAdmin(sender) && */(parameter.command === "c" || parameter.command === "clear")) {
-            if (parameter.players) {
-                for(let playerIndex = 0;playerIndex < parameter.players.length; playerIndex++) {
-                    const player = parameter.players[playerIndex];
-                    removeActiveParticles(player);
+        else if(isAdmin(sender) && (parameter.command === "r" || parameter.command === "reverse")) {
+            const playerName = parameter.player ?? sender.name
+            const request = {
+                sql : `SELECT DISTINCT block_history.*, PlayerConnections.PlayerName 
+                       FROM \`block_history\` 
+                       JOIN PlayerConnections 
+                       ON block_history.actor_id = PlayerConnections.PlayerID 
+                       WHERE PlayerName = ?  
+                       ORDER BY \`block_history\`.\`tick\` DESC
+                       LIMIT ? OFFSET ?`,
+                values : [playerName, parameter.count ?? 5, parameter.startingFrom ?? 0]
+            }
+            try {
+                const response = await BlockHistoryPLugin.database.query(request);
+                const tickInASec = TicksPerSecond
+                const tickInAMin = tickInASec*60
+                const tickInAnHour = tickInAMin*60
+                const tickInADay = tickInAnHour*24
+                let locations = []
+                for(let i = response.result.length-1; i+1; i--){
+                    const block_alteration = response.result[i]
+                    const timeOfBlockAlteration = system.currentTick - parseInt(block_alteration.tick)
+                    sendMessage(`${block_alteration.PlayerName} - [${block_alteration.x}, ${block_alteration.y}, ${block_alteration.z}]: ${block_alteration.before_id} -> ${block_alteration.after_id} - before: ${Math.floor(timeOfBlockAlteration/tickInADay)}d${Math.floor(timeOfBlockAlteration%tickInADay/tickInAnHour)}h${Math.floor(timeOfBlockAlteration%tickInAnHour/tickInAMin)}m${Math.floor(timeOfBlockAlteration%tickInAMin/tickInASec)}s`,'CMD - BlockHistory',sender);
+                    locations.push({x: block_alteration.x, y: block_alteration.y, z: block_alteration.z})
+                }
+                if(response.result == ""){
+                    sendMessage(`No changes were made by the player ${playerName}`,'CMD - BlockHistory',sender);
+                }
+                else{
+                    getEdgeLocations(locations, (loc,axis) => {
+                        addActiveParticles(loc,axis,sender);
+                    })
+                    sendMessage(`are you sure you want to reverse these changes?\n - !bh confirm to confirm`,'CMD - BlockHistory',sender);
+                    confirmationPerPlayer[sender.id] ??= { //that thing i've talked bout is with question marks instead
+                        player: sender,
+                        confirmation: response.result
+                    };
+                }
+
+            }
+            catch(error) {
+                sendMessage(`${error}`,'CMD - BlockHistory',sender);
+            }
+            
+        }
+        else if(/*isAdmin(sender) && */(parameter.command === "c" || parameter.command === "clear")) {
+            if(parameter.players){
+                for(let i = 0;i<parameter.players.length; i++){
+                    const player = parameter.players[i]
+                    removeActiveParticles(player)
                 }
             } else {
                 removeActiveParticles(sender);
             }
         }
-        else if (/*isAdmin(sender) && */(parameter.command === "ca" || parameter.command === "clearall")) {
+        else if(/*isAdmin(sender) && */(parameter.command === "ca" || parameter.command === "clearall")) {
             removeAllActiveParticles();
         }
         else {
@@ -138,7 +189,9 @@ function main() {
                 b/block - shows the changes made to block on [x], [y], [z] - parameters: count, startingFrom, location: x, location: y, location: z
                 p/player - shows the changes made by a player - parameters: count, startingFrom, player
                 i/inspect - gets you into inspector mode - when you place blocks it doesn't place them and instead shows you the changes made to that block
-                r/reverse - reverses actions of a player in specific time frame - parameters: player, operations
+                r/reverse - reverses actions of a player in specific time frame - parameters: count, startingFrom, player
+                bt/blockytools - show history of blockytools edits - parameters: count, startingFrom, player -- not ready yet
+                rbt/reversebt/reverseblockytools - reverses a blockytools edit using its id - parameters: count, startingFrom, player -- not ready yet
                 redo - reverses an action made by this plugin - parameters: ID
                 c/clear - clears all the particles generated by this plugin by a player
                 ca/clearall - clears all the particles generated by this plugin by everyone
