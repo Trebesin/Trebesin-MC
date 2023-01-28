7e4bce17d838b926806cdc87e7796ce0251ab3f1
tpkowastaken <protitom@gmail.com>
2023-01-28 18:37:48 +0100

    fixed imports

--- development_behavior_packs/Trebesin Mod [BP]/scripts/plugins/commands/commands.js
+++ development_behavior_packs/Trebesin Mod [BP]/scripts/plugins/commands/commands.js
@@ -2,7 +2,7 @@ import * as AdminWorker from "./workers/admin"
 import * as UserWorker from "./workers/user"
 import * as ItemsWorker from "./workers/items"
 import * as Debug from "../debug/debug"
-
+import * as DebugCommands from "./workers/debug"
 const debugAllowed = true
 
 async function main(){
@@ -12,6 +12,8 @@ async function main(){
     Debug.logMessage('   User commands set');
     ItemsWorker.main()
     Debug.logMessage('   item links to commands set');
+    DebugCommands.main();
+    Debug.logMessage('   debug commands set');
 }
 
 export {main, debugAllowed}
--- development_behavior_packs/Trebesin Mod [BP]/scripts/plugins/commands/workers/debug.js
+++ development_behavior_packs/Trebesin Mod [BP]/scripts/plugins/commands/workers/debug.js
@@ -88,3 +88,5 @@ function main(){
   })
 
 }
+
+export {main}
