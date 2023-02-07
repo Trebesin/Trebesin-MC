335fac639fa4f57ff7d5b7f54918d356fbef668f
tpkowastaken <protitom@gmail.com>
2023-02-07 17:30:10 +0100

    debugging

--- development_behavior_packs/Trebesin Mod [BP]/scripts/plugins/block_history/workers/commands.js
+++ development_behavior_packs/Trebesin Mod [BP]/scripts/plugins/block_history/workers/commands.js
@@ -269,7 +269,7 @@ function main(){
                             addActiveParticles(loc,axis,sender);
                         })
                     }
-                    sendMessage(`are you sure you want to reverse these changes?\n - !bh confirm to confirm or !bh cancel to cancel`,'CMD - BlockHistory',sender);
+                    system.run(sendMessage(`are you sure you want to reverse these changes?\n - !bh confirm to confirm or !bh cancel to cancel`,'CMD - BlockHistory',sender));
                     if(confirmationPerPlayer[sender.id]) delete confirmationPerPlayer[sender.id];
                     confirmationPerPlayer[sender.id] = {
                         player: sender,
