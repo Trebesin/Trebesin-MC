0403df2d608a97e3df6403b611d527fe261c3b7d
tpkowastaken <protitom@gmail.com>
2023-03-08 19:43:27 +0100

    unit testing didn't have a name

--- development_behavior_packs/Trebesin Mod [BP]/scripts/plugins/unit_testing/unit_testing.js
+++ development_behavior_packs/Trebesin Mod [BP]/scripts/plugins/unit_testing/unit_testing.js
@@ -1,6 +1,8 @@
 import { sendLogMessage } from "../debug/debug";
 import * as unitTesting from "./workers/unit_testing";
 
+const name = "unit testing"
+export {name}
 export async function main(){
     unitTesting.main()
     sendLogMessage('   unit testing loaded')
