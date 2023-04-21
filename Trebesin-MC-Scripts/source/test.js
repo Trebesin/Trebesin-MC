a625181aef0dc9458c88b186caeaf10b061d1a3e
PavelDobCZ23 <pavel23dob@gmail.com>
2023-04-21 23:58:08 +0200

    Sent generated source and sourcemaps!

---
+++ Trebesin-MC-Scripts/source/test.js
@@ -0,0 +1,10 @@
+"use strict";
+function testMain(n) {
+    return n * 2;
+}
+function sendUser(user) {
+    return user.user;
+}
+sendUser({ user: '' });
+
+//# sourceMappingURL=test.js.map
---
+++ Trebesin-MC-Scripts/source/test/test2.js
@@ -0,0 +1,6 @@
+"use strict";
+function test(n) {
+    return n * 2;
+}
+
+//# sourceMappingURL=test2.js.map
