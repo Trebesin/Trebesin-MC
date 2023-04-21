415ec739539d099c8c0428c67b061039c9eb8447
PavelDobCZ23 <pavel23dob@gmail.com>
2023-04-21 23:30:08 +0200

    ggg

--- Trebesin-MC-Scripts/scripts2/test.ts
+++ Trebesin-MC-Scripts/scripts2/test.ts
@@ -1,3 +1,13 @@
-function testMain (n: number): number {
+function testMain (n): number {
     return n * 2;
 }
+
+interface test {
+     user: string
+}
+
+function sendUser(user:test) {
+    return user.user
+}
+
+sendUser({k:1});
