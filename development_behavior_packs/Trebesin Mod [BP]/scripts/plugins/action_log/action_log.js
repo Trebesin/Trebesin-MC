05ee709ee919b4f3c7397467a6c3b6b9251044da
PavelDobCZ23 <pavel23dob@gmail.com>
2023-04-21 22:06:31 +0200

    cool

--- development_behavior_packs/Trebesin Mod [BP]/scripts/plugins/action_log/action_log.js
+++ development_behavior_packs/Trebesin Mod [BP]/scripts/plugins/action_log/action_log.js
@@ -4,6 +4,8 @@ import {world} from '@minecraft/server';
 
 export async function main() {
     world.events.playerJoin.subscribe(() => {
-
+        if (true) {
+            return;
+        }
     });
 }
