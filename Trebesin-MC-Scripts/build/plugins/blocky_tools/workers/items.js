2d0853ecf8caabdd04d9e6034b8f4d14f70524fc
PavelDobCZ23 <pavel23dob@gmail.com>
2023-09-20 18:06:43 +0200

    `ᅟAUTOMATED` - Sent generated source and sourcemaps!
    OS: linux Arch: x64 Node: v20.6.1 IP: 46.4.34.184

--- Trebesin-MC-Scripts/build/plugins/blocky_tools/workers/items.js
+++ Trebesin-MC-Scripts/build/plugins/blocky_tools/workers/items.js
@@ -19,6 +19,7 @@ export function main() {
             showChunkBorder[eventData.player.id] = 0;
     });
     Server.events.beforeItemStartUseOn.subscribe((eventData) => {
+        Debug.logMessage('HUH');
         if (eventData.itemStack.typeId === 'trebesin:bt_debug_stick')
             Mc.system.run(async () => {
                 /** @type {Mc.Player} */
