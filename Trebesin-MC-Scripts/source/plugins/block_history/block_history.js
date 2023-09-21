8675cc11bfdedbf118562d3ca016694cdd811ae6
PavelDobCZ23 <pavel23dob@gmail.com>
2023-09-21 16:26:26 +0200

    face

--- Trebesin-MC-Scripts/source/plugins/block_history/block_history.js
+++ Trebesin-MC-Scripts/source/plugins/block_history/block_history.js
@@ -189,13 +189,12 @@ export async function main() {
     const oldBlocksRegister = {};
     Mc.world.beforeEvents.itemUseOn.subscribe((eventData) => {
         Debug.logMessage('Before item use on');
-        oldBlocksRegister[vec3ToString(eventData.faceLocation)] = Blocks.copyBlockState(
-            eventData.block.dimension.getBlock(eventData.faceLocation),true
+        const offset = FACE_DIRECTIONS[eventData.blockFace];
+        const faceBlockLocation = VectorMath.sum(eventData.block.location,offset);
+        oldBlocksRegister[vec3ToString(faceBlockLocation)] = Blocks.copyBlockState(
+            eventData.block.dimension.getBlock(faceBlockLocation),true
         );
-        Debug.logMessage(JSON.stringify(eventData.faceLocation));
-        Debug.logMessage(JSON.stringify(oldBlocksRegister[vec3ToString(eventData.faceLocation)]));
         oldBlocksRegister[vec3ToString(eventData.block.location)] = Blocks.copyBlockState(eventData.block,true);
-        Debug.logMessage(JSON.stringify(oldBlocksRegister[vec3ToString(eventData.block.location)]));
     });
 
     Mc.world.afterEvents.itemStartUseOn.subscribe(async(eventData) => {
